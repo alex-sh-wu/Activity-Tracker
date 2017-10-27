b245573ae69c41cf7121cb07f1876da71fd6369e
Alex Wu <ashwu@uwaterloo.ca>
2017-10-26 23:36:57 -0400

    Added in Electron so that the webapp can also be an app

---
+++ index.js
@@ -0,0 +1,35 @@
+const {app, BrowserWindow} = require('electron')
+const path = require('path')
+const url = require('url')
+
+let win
+
+//type in './node_modules/.bin/electron .' on cmd to run electron
+
+function createWindow () {
+  win = new BrowserWindow({width: 1200, height: 700})
+
+  win.loadURL(url.format({
+    pathname: path.join(__dirname, 'html/utrack.html'),
+    protocol: 'file',
+    slashes: true
+  }))
+
+  win.on('closed', () => {
+    win = null
+  })
+}
+
+app.on('ready', createWindow)
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', () => {
+  if (win === null) {
+    createWindow()
+  }
+})
