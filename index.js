1dbfa81348c067f13e3d6380d5117d944ab02154
Alex Wu <ashwu@uwaterloo.ca>
2017-10-26 23:45:56 -0400

    Added in a minimum window size to the browser window

--- index.js
+++ index.js
@@ -7,7 +7,12 @@ let win
 //type in './node_modules/.bin/electron .' on cmd to run electron
 
 function createWindow () {
-  win = new BrowserWindow({width: 1200, height: 700})
+  win = new BrowserWindow({
+    width: 1200,
+    height: 700,
+    minWidth: 1000,
+    minHeight: 700
+  })
 
   win.loadURL(url.format({
     pathname: path.join(__dirname, 'html/utrack.html'),
